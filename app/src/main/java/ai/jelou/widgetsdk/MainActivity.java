dfa4bf75338f9b0ed20332e4ce7f65dafde7f932
Luigi Basantes <luigi@jelou.ai>
2023-03-17 00:17:33 -0500

    chore: added event connect to initilize widget and create user on init

--- app/src/main/java/ai/jelou/widgetsdk/MainActivity.java
+++ app/src/main/java/ai/jelou/widgetsdk/MainActivity.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import ai.jelou.widget.WidgetService;
+
 public class MainActivity extends AppCompatActivity {
+    public static final String JelouApiKey = "MTVlN2E0MmMtYzExYS00Mzk3LWI1ZGEtMjAyZGRiNjM3ZjBh";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        WidgetService widgetService = new WidgetService(JelouApiKey, "qwerty");
+        widgetService.setName("Luigi");
+        widgetService.Toast(MainActivity.this, "Hwllwlw ");
+
+        widgetService.connect(MainActivity.this);
     }
 }
